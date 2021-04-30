67e8e1dfa421653b7e61b1078580de39058aacce
Ghostemaner <DronByLive@mail.ru>
2021-04-30 20:42:20 +0300

    added registration.css and modal-registration.js //registration page has been finished // also modal.js was renamed into modal-index.js

---
+++ scripts/modal-registration.js
@@ -0,0 +1,19 @@
+let openMenuButton = document.getElementById("burger");
+
+let navigationMenu = document.getElementById("menu");
+
+let closeMenuButton = document.getElementById("close")
+
+
+
+openMenuButton.addEventListener("click", function () {
+    navigationMenu.classList.toggle("header__navigation--none");
+
+    navigationMenu.classList.toggle("header__navigation--block");
+});
+
+closeMenuButton.addEventListener("click", function () {
+    navigationMenu.classList.remove("header__navigation--block");
+
+    navigationMenu.classList.add("header__navigation--none");
+});
