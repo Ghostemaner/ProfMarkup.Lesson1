e16b088e669ee5aefaaea5911de610c2f2b506e1
Ghostemaner <DronByLive@mail.ru>
2021-05-27 18:19:32 +0300

    Page Catalog.html was finshed by Grid and bootstrap (used it bit) Website's carass ready but will be soon to add animation

--- scripts/modal-all.js
+++ scripts/modal-all.js
@@ -17,3 +17,34 @@ closeMenuButton.addEventListener("click", function () {
 
     navigationMenu.classList.add("header__navigation-none");
 });
+
+
+let filterButton = document.getElementById("filter-button");
+let accordion = document.getElementById("accordionExample");
+
+if (filterButton) {
+
+    filterButton.addEventListener("click", function () {
+        accordion.classList.toggle("accordion-none");
+        filterButton.classList.toggle("accordion-none");
+        accordion.classList.add("accordion-block");
+    });
+
+
+    let filterButtonSub = document.getElementById("filter-button-sub");
+
+    filterButtonSub.addEventListener("click", function () {
+        accordion.classList.toggle("accordion-none");
+        filterButton.classList.toggle("accordion-none");
+        accordion.classList.add("accordion-block");
+    });
+}
+
+
+let DropDown = document.getElementsByClassName("dropdown-menu");
+
+for (let Drop of DropDown) {
+    Drop.addEventListener("click", function (e) {
+        e.stopPropagation();
+    })
+}
